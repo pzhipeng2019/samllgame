bea2b4cff4ba1d156d7d9f64a30c66dc6a0e60be
xiaopg <pengzhipeng6@163.com>
2020-04-10 18:03:45 +0800

    首页

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,6 +7,22 @@ Page({
    */
   data: {
     productList:[],//数据列表
+    value: '',
+    banner: ['img1', 'img2', 'img3', 'img4','img5'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500,
+    indicatorColor:"#ffffff",
+    //倒計時
+    time: 30 * 60 * 60 * 1000,
+    timeData: {}
+  },
+  onChange(e) {
+    this.setData({
+      timeData: e.detail
+    });
   },
   /**
    * 获取product 数据
@@ -16,16 +32,16 @@ Page({
       success:res=>{
         console.log(res.data)
         this.setData({
-          productList:res.data,
+          productList: res.data,
         })
       }
     })
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log("onLoad")
     this.getProduct();//获取数据
   },
 
@@ -33,48 +49,49 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log("onReady")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log("onShow")
+    this.getProduct();//获取数据
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log("onHide")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log("onUnload")
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log("onPullDownRefresh")
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("onReachBottom")
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log("onShareAppMessage")
   }
 })
